30eeaa82a42a46f596f685d167d2499aa67ff850
nctriet <trietnguyencao02@gmail.com>
2026-06-14 16:49:30 +0700

    remove admin feature

--- app/admin/tests/page.tsx
+++
@@ -1,8 +0,0 @@
-import { getAllReadingTests } from "@/lib/data";
-import AdminTestListClient from "./AdminTestListClient";
-
-export default async function AdminTestsPage() {
-  const tests = await getAllReadingTests();
-
-  return <AdminTestListClient tests={tests} />;
-}
